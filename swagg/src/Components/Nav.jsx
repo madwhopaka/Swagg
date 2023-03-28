93de2b6e2a941436a7048d622e89f4f0a7861046
madhu paka <madhupaka11@gmail.com>
2023-03-28 15:00:00 +0530

    otplessfinal3

--- swagg/src/Components/Nav.jsx
+++ swagg/src/Components/Nav.jsx
@@ -27,12 +27,7 @@ function Nav() {
   const handleLogout = async () => {
     dispatch(logout());
     await persistor.purge();
-    var element = document.getElementById("otpless");
-    // Check if the element exists
-    if (element) {
-      // Remove the element from the DOM
-      element.parentNode.removeChild(element);
-    }
+    localStorage.clear();
     // Redirect the user to the login page
     window.location.href = '/';
     dispatch(clearCart());
