c67c16ed78d1cdb93adb21d6261e5b870589787d
Madhukumar Paka <74057522+madwhopaka@users.noreply.github.com>
2023-04-07 17:06:27 +0530

    Update requestMethods.js

--- swagg/src/requestMethods.js
+++ swagg/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://swagg-backend-production.up.railway.app/api";
+const BASE_URL = "swagg-backend-production.up.railway.app/api";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTJmNjIzNDI3Njk2MjBkMjlkY2Q4YiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NDIyMDIxMSwiZXhwIjoxNjQ0NDc5NDExfQ.pxtAm7KE8sMOB7oMYduyscCiDwWjNXkaDSyFKvk2ong";
 
